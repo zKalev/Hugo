51bc8c8d9cb473d3cbe30fed231bdc82284ed7d7
stuckata <stu.kirkov@gmail.com>
2014-10-18 22:05:46 +0300

    player hitpoint label added to GameField info

--- Hugo/HugoWF/UI/GameFieldForm.Designer.cs
+++ Hugo/HugoWF/UI/GameFieldForm.Designer.cs
@@ -29,24 +29,65 @@
         private void InitializeComponent()
         {
 			this.AdditionalInfoPanel = new System.Windows.Forms.Panel();
-			this.PlayerColor = new System.Windows.Forms.TextBox();
-			this.PlayerPoints = new System.Windows.Forms.TextBox();
-			this.PlayerLabel = new System.Windows.Forms.TextBox();
 			this.PlayerInfo = new System.Windows.Forms.TextBox();
+			this.PlayerLabel = new System.Windows.Forms.TextBox();
+			this.PlayerPoints = new System.Windows.Forms.TextBox();
+			this.PlayerColor = new System.Windows.Forms.TextBox();
+			this.PlayerPointsLabel = new System.Windows.Forms.TextBox();
 			this.AdditionalInfoPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// AdditionalInfoPanel
 			// 
 			this.AdditionalInfoPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.AdditionalInfoPanel.Controls.Add(this.PlayerPointsLabel);
 			this.AdditionalInfoPanel.Controls.Add(this.PlayerInfo);
 			this.AdditionalInfoPanel.Controls.Add(this.PlayerLabel);
 			this.AdditionalInfoPanel.Controls.Add(this.PlayerPoints);
 			this.AdditionalInfoPanel.Controls.Add(this.PlayerColor);
+			this.AdditionalInfoPanel.ForeColor = System.Drawing.SystemColors.Window;
 			this.AdditionalInfoPanel.Location = new System.Drawing.Point(584, 0);
 			this.AdditionalInfoPanel.Name = "AdditionalInfoPanel";
 			this.AdditionalInfoPanel.Size = new System.Drawing.Size(342, 587);
 			this.AdditionalInfoPanel.TabIndex = 0;
+			// 
+			// PlayerInfo
+			// 
+			this.PlayerInfo.Enabled = false;
+			this.PlayerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PlayerInfo.ForeColor = System.Drawing.SystemColors.Window;
+			this.PlayerInfo.Location = new System.Drawing.Point(31, 50);
+			this.PlayerInfo.Name = "PlayerInfo";
+			this.PlayerInfo.ReadOnly = true;
+			this.PlayerInfo.Size = new System.Drawing.Size(286, 32);
+			this.PlayerInfo.TabIndex = 3;
+			// 
+			// PlayerLabel
+			// 
+			this.PlayerLabel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.PlayerLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.PlayerLabel.Enabled = false;
+			this.PlayerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PlayerLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.PlayerLabel.Location = new System.Drawing.Point(31, 12);
+			this.PlayerLabel.Name = "PlayerLabel";
+			this.PlayerLabel.ReadOnly = true;
+			this.PlayerLabel.Size = new System.Drawing.Size(286, 25);
+			this.PlayerLabel.TabIndex = 2;
+			this.PlayerLabel.Text = "Next player:";
+			this.PlayerLabel.TextChanged += new System.EventHandler(this.PlayerLabel_TextChanged);
+			// 
+			// PlayerPoints
+			// 
+			this.PlayerPoints.Enabled = false;
+			this.PlayerPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PlayerPoints.ForeColor = System.Drawing.SystemColors.Window;
+			this.PlayerPoints.Location = new System.Drawing.Point(31, 157);
+			this.PlayerPoints.Name = "PlayerPoints";
+			this.PlayerPoints.ReadOnly = true;
+			this.PlayerPoints.Size = new System.Drawing.Size(286, 32);
+			this.PlayerPoints.TabIndex = 1;
+			this.PlayerPoints.TextChanged += new System.EventHandler(this.PlayerPoints_TextChanged);
 			// 
 			// PlayerColor
 			// 
@@ -60,42 +101,19 @@
 			this.PlayerColor.TabIndex = 0;
 			this.PlayerColor.TextChanged += new System.EventHandler(this.PlayerColor_TextChanged);
 			// 
-			// PlayerPoints
+			// PlayerPointsLabel
 			// 
-			this.PlayerPoints.Enabled = false;
-			this.PlayerPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.PlayerPoints.ForeColor = System.Drawing.SystemColors.Window;
-			this.PlayerPoints.Location = new System.Drawing.Point(31, 126);
-			this.PlayerPoints.Name = "PlayerPoints";
-			this.PlayerPoints.ReadOnly = true;
-			this.PlayerPoints.Size = new System.Drawing.Size(286, 32);
-			this.PlayerPoints.TabIndex = 1;
-			this.PlayerPoints.TextChanged += new System.EventHandler(this.PlayerPoints_TextChanged);
-			// 
-			// PlayerLabel
-			// 
-			this.PlayerLabel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-			this.PlayerLabel.Enabled = false;
-			this.PlayerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.PlayerLabel.ForeColor = System.Drawing.SystemColors.Window;
-			this.PlayerLabel.Location = new System.Drawing.Point(31, 12);
-			this.PlayerLabel.Name = "PlayerLabel";
-			this.PlayerLabel.ReadOnly = true;
-			this.PlayerLabel.Size = new System.Drawing.Size(286, 32);
-			this.PlayerLabel.TabIndex = 2;
-			this.PlayerLabel.Text = "Next player:";
-			this.PlayerLabel.TextChanged += new System.EventHandler(this.PlayerLabel_TextChanged);
-			// 
-			// PlayerInfo
-			// 
-			this.PlayerInfo.Enabled = false;
-			this.PlayerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.PlayerInfo.ForeColor = System.Drawing.SystemColors.Window;
-			this.PlayerInfo.Location = new System.Drawing.Point(31, 50);
-			this.PlayerInfo.Name = "PlayerInfo";
-			this.PlayerInfo.ReadOnly = true;
-			this.PlayerInfo.Size = new System.Drawing.Size(286, 32);
-			this.PlayerInfo.TabIndex = 3;
+			this.PlayerPointsLabel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.PlayerPointsLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.PlayerPointsLabel.Enabled = false;
+			this.PlayerPointsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PlayerPointsLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.PlayerPointsLabel.Location = new System.Drawing.Point(31, 126);
+			this.PlayerPointsLabel.Name = "PlayerPointsLabel";
+			this.PlayerPointsLabel.ReadOnly = true;
+			this.PlayerPointsLabel.Size = new System.Drawing.Size(286, 25);
+			this.PlayerPointsLabel.TabIndex = 4;
+			this.PlayerPointsLabel.Text = "Player hitpoints:";
 			// 
 			// GameFieldForm
 			// 
@@ -121,6 +139,7 @@
 		private System.Windows.Forms.TextBox PlayerPoints;
 		private System.Windows.Forms.TextBox PlayerLabel;
 		private System.Windows.Forms.TextBox PlayerInfo;
+		private System.Windows.Forms.TextBox PlayerPointsLabel;
 
     }
 }
